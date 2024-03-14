fccef7f399139a2d288b46ca00bda9c8192811b5
Jdave1125 <Jdave1125@gmail.com>
2024-03-14 17:46:48 -0400

    checking to see if a link solves the issue

--- src/welcome.jsx
+++ src/welcome.jsx
@@ -1,7 +1,7 @@
 import {motion} from 'framer-motion'
 import AnimatedPage from './AnimatedPage';
 import ParticlesComp from './particles';
-
+import { Link } from 'react-router-dom';
 const Welcome = () => {
   return (
     <AnimatedPage>
@@ -11,7 +11,7 @@ const Welcome = () => {
     {/* initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}> */}
       <h1 className="jeremy"> JEREMY DAVID </h1>
       <h4 className="subline">SOFTWARE ENGINEER</h4>
-      <a className="start" href="/home">ENTER</a>
+      <Link to="/home" className="start" href="/home">ENTER</Link>
       <p className="tagline">© EST 1996 - ALBANY, NY. All Rights Reserved. You are who you choose to be.</p>
       <ParticlesComp />
     </motion.div>
